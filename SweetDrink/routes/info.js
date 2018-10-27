29ab6a14fd749422931a820efedce8e4d1f4fb19
sannchez-Mx <sannchez-mx@outlook.com>
2018-10-26 19:27:46 -0500

    mas vistas

--- SweetDrink/routes/info.js
+++ SweetDrink/routes/info.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const router  = express.Router();
 
+router.get("/aboutUs", (req, res)=>{
+  res.render("aboutUs");
+});
+
+router.get("/contact", (req, res)=>{
+  res.render("contact");
+});
 
 module.exports = router;
