4bca4d6633789986fcadd5e0557a01d8d6ebaaef
sannchez-Mx <sannchez-mx@outlook.com>
2018-11-02 21:52:53 -0600

    vista menu3

--- SweetDrink/bin/seeds.js
+++ SweetDrink/bin/seeds.js
@@ -139,25 +139,248 @@ const brandy = [
     image: "https://sistema.vampiritos.com/pos/img/productos/593.jpg",
     name : "Torres 5",
     type: "Brandy",
-    Qty: "700",
+    Qty: "700ml",
     price: 250.00
   },
   {
     image: "https://bodegasalianza.vteximg.com.br/arquivos/ids/164143-800-800/30022-1.jpg?v=636588733158930000",
     name : "Torres spiced",
     type: "Brandy",
-    Qty: "990",
+    Qty: "990ml",
     price: 200.00
   },
   {
     image: "https://bodegasalianza.vteximg.com.br/arquivos/ids/159778-1000-1000/0063-1.jpg?v=636347883107370000",
     name : "Azteca de oro",
     type: "Brandy",
-    Qty: "700",
+    Qty: "700ml",
     price: 145.00
   }
 ];
 
+const ron = [
+  {
+    image: "https://sistema.vampiritos.com/pos/img/productos/20.jpg",
+    name : "Appleton Estate V/X",
+    type: "Ron",
+    Qty: "750ml",
+    price: 315.00
+  },
+  {
+    image: "https://sistema.vampiritos.com/pos/img/productos/2.jpg",
+    name : "Bacardi Bco. (Pat)",
+    type: "Ron",
+    Qty: "1.75 lt",
+    price: 345.00
+  },
+  {
+    image: "https://sistema.vampiritos.com/pos/img/productos/1.jpg",
+    name : "Bacardi Bco.",
+    type: "Ron",
+    Qty: "1 lt",
+    price: 250.00
+  },
+  {
+    image: "https://sistema.vampiritos.com/pos/img/productos/10.jpg",
+    name : "Captain Morgan",
+    type: "Ron",
+    Qty: "750ml",
+    price: 200.00
+  },
+  {
+    image: "https://sistema.vampiritos.com/pos/img/productos/16.jpg",
+    name : "Kraken",
+    type: "Ron",
+    Qty: "1 lt",
+    price: 235.00
+  }
+];
+
+const vodka = [
+  {
+    image: "https://sistema.vampiritos.com/pos/img/productos/34.jpg",
+    name : "Absolut Azul",
+    type: "Vodka",
+    Qty: "750ml",
+    price: 345.00
+  },
+  {
+    image: "https://sistema.vampiritos.com/pos/img/productos/38.jpg",
+    name : "Grey Goose",
+    type: "Vodka",
+    Qty: "700ml",
+    price: 850.00
+  },
+  {
+    image: "https://sistema.vampiritos.com/pos/img/productos/37.jpg",
+    name : "Smirnoff.",
+    type: "Vodka",
+    Qty: "1 lt",
+    price: 280.00
+  },
+  {
+    image: "https://sistema.vampiritos.com/pos/img/productos/598.jpg",
+    name : "Smirnoff Tamarindo",
+    type: "Vodka",
+    Qty: "750ml",
+    price: 245.00
+  },
+  {
+    image: "https://sistema.vampiritos.com/pos/img/productos/39.jpg",
+    name : "Stolichnaya",
+    type: "Vodka",
+    Qty: "750ml",
+    price: 360.00
+  }
+];
+const whiskey = [
+  {
+    image: "https://sistema.vampiritos.com/pos/img/productos/87.jpg",
+    name : "Buchanans",
+    type: "Whiskey",
+    Qty: "700ml",
+    price: 795.00
+  },
+  {
+    image: "https://sistema.vampiritos.com/pos/img/productos/83.jpg",
+    name : "Chivas Regal",
+    type: "Whiskey",
+    Qty: "750ml",
+    price: 725.00
+  },
+  {
+    image: "https://sistema.vampiritos.com/pos/img/productos/85.jpg",
+    name : "J.W. Etiqueta Neg.",
+    type: "Whiskey",
+    Qty: "750ml",
+    price: 840.00
+  },
+  {
+    image: "https://sistema.vampiritos.com/pos/img/productos/84.jpg",
+    name : "J.W. Etiqueta Roja",
+    type: "Whiskey",
+    Qty: "750ml",
+    price: 385.00
+  },
+  {
+    image: "https://sistema.vampiritos.com/pos/img/productos/86.jpg",
+    name : "Jack Daniels",
+    type: "Whiskey",
+    Qty: "700ml",
+    price: 485.00
+  }
+];
+const ginebra = [
+  {
+    image: "https://sistema.vampiritos.com/pos/img/productos/596.jpg",
+    name : "BEEFEATER",
+    type: "Ginebra",
+    Qty: "700ml",
+    price: 525.00
+  },
+  {
+    image: "https://sistema.vampiritos.com/pos/img/productos/174.jpg",
+    name : "Bombay Shapphire",
+    type: "Ginebra",
+    Qty: "700ml",
+    price: 580.00
+  },
+  {
+    image: "https://sistema.vampiritos.com/pos/img/productos/179.jpg",
+    name : "Hendricks",
+    type: "Ginebra",
+    Qty: "750ml",
+    price: 845.00
+  },
+  {
+    image: "https://sistema.vampiritos.com/pos/img/productos/595.jpg",
+    name : "TANQUERAY",
+    type: "Ginebra",
+    Qty: "750ml",
+    price: 490.00
+  },
+  {
+    image: "https://bodegasalianza.vteximg.com.br/arquivos/ids/158159-1000-1000/26714-1.jpg?v=636162013367830000",
+    name : "Thomas Dakin",
+    type: "Ginebra",
+    Qty: "700ml",
+    price: 550.00
+  }
+];
+const mezclador = [
+  {
+    image: "https://sistema.vampiritos.com/pos/img/productos/69.jpg",
+    name : "Agua Mineral",
+    type: "Mezclador",
+    Qty: "2 lt",
+    price: 40.00
+  },
+  {
+    image: "https://sistema.vampiritos.com/pos/img/productos/70.jpg",
+    name : "Agua Natural Ciel ",
+    type: "Mezclador",
+    Qty: "1.5 lt",
+    price: 25.00
+  },
+  {
+    image: "https://sistema.vampiritos.com/pos/img/productos/71.jpg",
+    name : "Agua Quina",
+    type: "Mezclador",
+    Qty: "6 pack",
+    price: 85.00
+  },
+  {
+    image: "https://sistema.vampiritos.com/pos/img/productos/73.jpg",
+    name : "B:oost",
+    type: "Mezclador",
+    Qty: "120ml",
+    price: 40.00
+  },
+  {
+    image: "https://sistema.vampiritos.com/pos/img/productos/63.jpg",
+    name : "Coca-Cola",
+    type: "Mezclador",
+    Qty: "2 lt",
+    price: 40.00
+  }
+];
+const jugos = [
+  {
+    image: "https://sistema.vampiritos.com/pos/img/productos/131.jpg",
+    name : "Clamato",
+    type: "Jugos",
+    Qty: "1 lt",
+    price: 55.00
+  },
+  {
+    image: "https://sistema.vampiritos.com/pos/img/productos/130.jpg",
+    name : "Jugo de Arandano",
+    type: "Jugos",
+    Qty: "1 lt",
+    price: 45.00
+  },
+  {
+    image: "https://sistema.vampiritos.com/pos/img/productos/127.jpg",
+    name : "Jugo de Naranja ",
+    type: "Jugos",
+    Qty: "1 lt",
+    price: 40.00
+  },
+  {
+    image: "https://sistema.vampiritos.com/pos/img/productos/129.jpg",
+    name : "Jugo de Piña",
+    type: "Jugos",
+    Qty: "1 lt",
+    price: 40.00
+  },
+  {
+    image: "https://sistema.vampiritos.com/pos/img/productos/128.jpg",
+    name : "Jugo de uva",
+    type: "Jugos",
+    Qty: "1 lt",
+    price: 40.00
+  }
+];
 /*Producto.create(cervezas, (err) => {
   if (err) throw(err)
   console.log(`Created ${cervezas.length} cervezas`)
@@ -175,10 +398,46 @@ Producto.create(tequila, (err) => {
   console.log(`Created ${tequila.length} tequilas`)
   // cerrar conexion a la BD
   mongoose.connection.close()
-});*/
+});
 Producto.create(brandy, (err) => {
   if (err) throw(err)
   console.log(`Created ${brandy.length} Brandys`)
   // cerrar conexion a la BD
   mongoose.connection.close()
 })
+Producto.create(ron, (err) => {
+  if (err) throw(err)
+  console.log(`Created ${ron.length} Rones`)
+  // cerrar conexion a la BD
+  mongoose.connection.close()
+})
+Producto.create(vodka, (err) => {
+  if (err) throw(err)
+  console.log(`Created ${vodka.length} Vodkas`)
+  // cerrar conexion a la BD
+  mongoose.connection.close()
+})
+Producto.create(whiskey, (err) => {
+  if (err) throw(err)
+  console.log(`Created ${whiskey.length} Whiskeys`)
+  // cerrar conexion a la BD
+  mongoose.connection.close()
+})
+Producto.create(ginebra, (err) => {
+  if (err) throw(err)
+  console.log(`Created ${ginebra.length} Ginebras`)
+  // cerrar conexion a la BD
+  mongoose.connection.close()
+})
+Producto.create(mezclador, (err) => {
+  if (err) throw(err)
+  console.log(`Created ${mezclador.length} Mezcladores`)
+  // cerrar conexion a la BD
+  mongoose.connection.close()
+})
+Producto.create(jugos, (err) => {
+  if (err) throw(err)
+  console.log(`Created ${jugos.length} Jugos`)
+  // cerrar conexion a la BD
+  mongoose.connection.close()
+})*/
--- SweetDrink/routes/menu.js
+++ SweetDrink/routes/menu.js
@@ -3,17 +3,37 @@ const router  = express.Router();
 const Producto = require('../models/Producto');
 
 router.get("/productos", (req, res) =>{
-  Producto.find({type: "Tequila"})
-  .then(tequila=>{
-    Producto.find({type: "Vino"})
-  .then(vino=>{
-    console.log(vino)
-    Producto.find({type: "cerveza"})
-    .then(cerveza=>{
-      res.render("menu", {cerveza, vino, tequila})
-    })
-  })
-  })
+  Producto.find({type: "Jugos"})
+  .then(jugos=>{
+    Producto.find({type: "Mezclador"})
+  .then(mezclador=>{
+    Producto.find({type: "Ginebra"})
+  .then(ginebra=>{
+    Producto.find({type: "Whiskey"})
+  .then(whiskey=>{
+    Producto.find({type: "Vodka"})
+  .then(vodka=>{
+    Producto.find({type: "Ron"})
+    .then(ron=>{
+      Producto.find({type: "Brandy"})
+    .then(brandy=>{
+      Producto.find({type: "Tequila"})
+    .then(tequila=>{
+      Producto.find({type: "Vino"})
+    .then(vino=>{
+      Producto.find({type: "cerveza"})
+      .then(cerveza=>{
+        res.render("menu", {cerveza, vino, tequila, brandy, ron, vodka, whiskey, ginebra, mezclador, jugos})
+      });
+    });
+    });
+    });
+    });
+  });
+  });
+  });
+  });
+  });
 });
 
 module.exports = router;
